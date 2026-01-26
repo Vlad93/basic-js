6d1160169eab5419dd8d4a7da271f0439d14360d
Vlad93 <selukvlad@mail.ru>
2026-01-26 13:50:44 +0100

    feat: resolve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,66 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(boolean) {
+    if(typeof boolean === 'boolean' && !boolean) {
+      this.modification = 'reverse';
+    } else {
+      this.modification = 'direct';
+    }
+  }
+  encrypt(str, key) {
+    const errorMessage = 'Incorrect arguments!';
+    if(!str || !key) {
+      throw new Error(errorMessage);
+    }
+    const upperKey = key.toUpperCase();
+    const minCharCode = 'A'.charCodeAt(0);
+    const maxCharCode = 'Z'.charCodeAt(0);
+    const isLatinChar = (char) => {
+      return minCharCode <= char.charCodeAt(0) && char.charCodeAt(0) <= maxCharCode;
+    }
+    let result = str.toUpperCase().split('');
+    let idx = 0;
+    for (let i = 0; i < result.length; i++) {
+      if (isLatinChar(result[i])) {
+        const curCharCode = result[i].charCodeAt(0);
+        const keyCharCode = idx < upperKey.length ? upperKey[idx].charCodeAt(0) : upperKey[idx % upperKey.length].charCodeAt(0);      
+        const shift = keyCharCode - minCharCode;
+        let newCode = curCharCode + shift;
+        newCode = newCode <= maxCharCode ? newCode : minCharCode + (newCode - maxCharCode - 1);
+        result[i] = String.fromCharCode(newCode);
+        idx++;
+      }
+    }
+    
+    return this.modification === 'direct' ? result.join('') : result.reverse().join('');      
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  decrypt(str, key) {
+    const errorMessage = 'Incorrect arguments!';
+    if(!str || !key) {
+      throw new Error(errorMessage);
+    }
+    const upperKey = key.toUpperCase();
+    const minCharCode = 'A'.charCodeAt(0);
+    const maxCharCode = 'Z'.charCodeAt(0);
+    const isLatinChar = (char) => {
+      return minCharCode <= char.charCodeAt(0) && char.charCodeAt(0) <= maxCharCode;
+    }
+    let result = str.toUpperCase().split('');
+    let idx = 0;
+    for (let i = 0; i < result.length; i++) {
+      if (isLatinChar(result[i])) {
+        const curCharCode = result[i].charCodeAt(0);
+        const keyCharCode = idx < upperKey.length ? upperKey[idx].charCodeAt(0) : upperKey[idx % upperKey.length].charCodeAt(0);      
+        const shift = keyCharCode - minCharCode;
+        let newCode = curCharCode - shift;
+        newCode = newCode >= minCharCode ? newCode : maxCharCode - (minCharCode - newCode - 1);
+        result[i] = String.fromCharCode(newCode);
+        idx++;
+      }
+    }
+    return this.modification === 'direct' ? result.join('') : result.reverse().join('');
   }
 }
 
